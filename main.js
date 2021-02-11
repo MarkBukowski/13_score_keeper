5262c0de98290774cb6c42ee7098ded0d60e1f6d
MarkBukowski <MarkBukowski@users.noreply.github.com>
2021-02-11 19:21:07 +0200

    Added js logic

--- main.js
+++ main.js
@@ -1,26 +1,71 @@
+// BUTTONS
 const pOne = document.querySelector('.player-one');
 const pTwo = document.querySelector('.player-two');
 const reset = document.querySelector('.reset');
-const HTML = document.querySelector('.score');
+
+// SCORES HTML
+const pOneScore = document.querySelector('.pOne-score');
+const pTwoScore = document.querySelector('.pTwo-score');
+
+// MAX SCORE
+const scoreGoal = document.querySelector('#playto');
+
+let winScore = 5;
 let scoreOne = 0;
 let scoreTwo = 0;
+let gameOver = false;
+
 
 pOne.addEventListener('click', () => {
-    HTML.innerText = `${scoreOne++} to ${scoreTwo}`
+    if (!gameOver) {
+        scoreOne++;
+        if (scoreOne === winScore) {
+            gameOver = true;
+            pOneScore.classList.add('winner');
+            pTwoScore.classList.add('loser');
+            pOne.disabled = true;
+            pTwo.disabled = true;
+        }
+        pOneScore.innerText = scoreOne;
+    }
+
+
+    // scoreOne !== winScore ? scoreOne++ : false;
+    // pOneScore.innerText = scoreOne;
+    // scoreOne === winScore ? gameOver = true : false;
+    // console.log(gameOver);
 });
 
 pTwo.addEventListener('click', () => {
-    HTML.innerText = `${scoreOne} to ${scoreTwo++}`
+    if (!gameOver) {
+        scoreTwo++;
+        if (scoreTwo === winScore) {
+            gameOver = true;
+            pTwoScore.classList.add('winner');
+            pOneScore.classList.add('loser');
+            pOne.disabled = true;
+            pTwo.disabled = true;
+        }
+        pTwoScore.textContent = scoreTwo;
+    }
 });
 
+scoreGoal.addEventListener('change', () => {
+    winScore = parseInt(this.value);
+    resetScore();
+});
 
-reset.addEventListener('click', () => {
+reset.addEventListener('click', (resetScore));
+
+function resetScore() {
+    gameOver = false;
     scoreOne = 0;
     scoreTwo = 0;
-    HTML.innerHTML = `<h2 class="score">
-    <p class="pOneScore">${scoreOne}</p> to
-    <p class="pTwoScore">${scoreTwo}</p>
-</h2>`
-});
+    pOneScore.innerText = scoreOne;
+    pTwoScore.innerText = scoreTwo;
+    pOneScore.classList.remove('winner', 'loser');
+    pTwoScore.classList.remove('winner', 'loser');
+    pOne.disabled = false;
+    pTwo.disabled = false;
+};
 
-resetScore();
